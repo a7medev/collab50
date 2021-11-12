62706b34ba749147e8f3c33e9f1a5f62ccdc1a8b
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-12 20:23:38 +0200

    feat: login API route

---
+++ pages/api/login.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import * as Yup from 'yup';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import prisma from '../../config/prisma';
+
+const userSchema = Yup.object({
+  username: Yup.string().label('Username').trim().lowercase().required(),
+  password: Yup.string().label('Password').required(),
+}).required();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const data = await userSchema.validate(req.body);
+
+    const user = await prisma.user.findFirst({
+      where: { username: data.username },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid username or password',
+      });
+    }
+
+    const passwordsMatch = await bcrypt.compare(data.password, user.password);
+
+    if (!passwordsMatch) {
+      return res.status(401).json({
+        message: 'Invalid username or password',
+      });
+    }
+
+    const accessToken = jwt.sign(
+      { userId: user.id },
+      process.env.ACCESS_TOKEN_SECRET as string,
+      { expiresIn: '7d' }
+    );
+
+    res.status(201).json({
+      data: {
+        user: { id: user.id, name: user.name, username: user.username },
+        accessToken,
+      },
+    });
+  } catch (error) {
+    const err = error as Yup.ValidationError;
+    if (err.name === 'ValidationError') {
+      return res.status(400).json({ message: err.message });
+    }
+
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+}
