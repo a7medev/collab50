ecf02d7356d25a66c105de6a89313bc3f10c7b2d
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-16 18:44:16 +0200

    feat: get single project API route

---
+++ pages/api/projects/[id].ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../config/prisma';
+import userInCookies from '../../../utils/user-in-cookies';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const user = await userInCookies(req.cookies);
+
+  if (!user) {
+    return res.status(401).json({
+      message: 'You must be logged in to access projects.',
+    });
+  }
+
+  const projectId = +req.query.id;
+
+  if (isNaN(projectId) || projectId < 0) {
+    return res.status(400).json({
+      message: 'Invalid project ID',
+    });
+  }
+
+  const project = await prisma.usersOnProject.findFirst({
+    where: {
+      projectId,
+      userId: user.id,
+    },
+    include: { project: true },
+  });
+
+  if (!project) {
+    return res.status(404).json({ message: 'Project not found' });
+  }
+
+  res.json({ data: { project } });
+}
