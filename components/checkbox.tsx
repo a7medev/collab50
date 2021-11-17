7f2ff8d927a2182ad8cd17324abc00b17af82e07
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-17 11:52:20 +0200

    chore: add custom checkbox component

---
+++ components/checkbox.tsx
@@ -0,0 +1,32 @@
+import '@reach/checkbox/styles.css';
+import { CustomCheckbox, CustomCheckboxProps } from '@reach/checkbox';
+import cn from '../utils/classnames';
+
+interface CheckboxProps extends CustomCheckboxProps {}
+
+const Checkbox: React.FC<CheckboxProps> = ({ children, ...props }) => {
+  return (
+    <label className="inline-flex items-center">
+      <CustomCheckbox {...props}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 13l4 4L19 7"
+          />
+        </svg>
+      </CustomCheckbox>
+
+      {children}
+    </label>
+  );
+};
+
+export default Checkbox;
